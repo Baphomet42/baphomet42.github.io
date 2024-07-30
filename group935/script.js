b4e91e708e2bc7168124e27a1d81642b5125e034
Baphomet42 <71295013+Baphomet42@users.noreply.github.com>
2024-07-30 00:41:13 -0500

    .

--- group935/script.js
+++ group935/script.js
@@ -231,6 +231,11 @@ function pageRev2(data) {
 
 function pageRev1(data) {
     var page = ''
+
+    if(data.background) {
+        page += '<div id="parallax-background" style="background-image: url('+data.background+')"></div>'
+    }
+    
     page += '<div class="section">'
     page += '<h1 class="center">'+data.name+'</h1>'
     page += '<h3 class="center">'+data.subtext+'</h3>'
